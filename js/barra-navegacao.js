68a0548dad8faea68fa694b12ad233b8412aa326
Leonardo Romano Camargo <leonardorcamargo@gmail.com>
2017-12-08 08:10:24 -0200

    Adicionado rotina para carregar controle de clientes e listar clientes na tabela

--- js/barra-navegacao.js
+++ js/barra-navegacao.js
@@ -14,5 +14,17 @@ function carregaControleCliente(){
     var main = document.querySelector("#idMainHome");
     main.setAttribute("w3-include-html","view/cliente.html");
     w3.includeHTML();
-    main.remove;
+}
+
+var botaoTeste = document.querySelector("#idBotaoTeste");
+
+botaoTeste.addEventListener("click",function(){
+    excluiConteudo();
+});
+
+function excluiConteudo(){
+    var main = document.querySelector("#idMainHome");
+    while (main.firstChild) {
+        main.removeChild(main.firstChild);
+    }
 }
---
+++ js/controle-cliente.js
@@ -0,0 +1,72 @@
+function buscarClientes(){
+    console.log("teste");
+    
+    var clientes = [
+        {
+            razaosocial:"Razão social 1",
+            nomefantasia:"Nome fantasia 1",
+            endereco:"Endereço 1",
+            cidade:"Avaré",
+            bairro:"Centro",
+            estado:"SP",
+            contato:"Luciano"
+        },
+        {
+            razaosocial:"Razão Social 2",
+            nomefantasia:"Nome fantasia 2",
+            endereco:"Endereço 2",
+            bairro:"Centro",
+            cidade:"São Paulo",            
+            estado:"SP",
+            contato:"Rafael"
+        },
+        {
+            razaosocial:"Razão Social 3",
+            nomefantasia:"Nome Fantasia 3",
+            endereco:"Endereço 4",
+            bairro:"Centro",
+            cidade:"Fartura",
+            estado:"SP",
+            contato:"Willians"
+        },
+        {
+            razaosocial:"Razão Social 4",
+            nomefantasia:"Nome Fantasia 4",
+            endereco:"Endereço 5",
+            bairro:"Centro",
+            cidade:"Carlópolis",
+            estado:"PR",
+            contato:"Gustavo"
+        }
+    ];
+
+    carregarTabela(clientes);
+}
+
+function carregarTabela(clientes){
+    var tabela = document.querySelector("#idTabelaClientes");
+    var tbody = tabela.querySelector("tbody");
+    
+    clientes.forEach(function(cliente) {
+        tbody.appendChild(montaTr(cliente));
+    });
+}
+
+function montaTr(cliente){
+    var tr = document.createElement("tr");
+    tr.appendChild(montaTd(cliente.razaosocial));
+    tr.appendChild(montaTd(cliente.nomefantasia));
+    tr.appendChild(montaTd(cliente.endereco));
+    tr.appendChild(montaTd(cliente.cidade));
+    tr.appendChild(montaTd(cliente.bairro));
+    tr.appendChild(montaTd(cliente.estado));
+    tr.appendChild(montaTd(cliente.contato));
+    return tr;
+}
+
+
+function montaTd(value){
+    var td = document.createElement("td");
+    td.textContent = value;
+    return td;
+}
